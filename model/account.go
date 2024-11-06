ea64f447f5f473abe46dabd19afdbd50c36a9662
rizkydarms <rizky.darms55@gmail.com>
2024-11-06 10:19:54 +0700

    Edit add: balance

--- model/account.go
+++ model/account.go
@@ -3,6 +3,7 @@ package model
 type Account struct {
 	AccountID int64  `json:"account_id" gorm:"primaryKey;autoIncrement;<-:false"`
 	Name      string `json:"name"`
+	Balance   string `json:"balance"`
 }
 
 // func (Account) TableName() string {
