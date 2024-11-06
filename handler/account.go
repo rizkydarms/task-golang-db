3bd4e2dd42addd4abe9fefb1abc17a6475676f49
rizkydarms <rizky.darms55@gmail.com>
2024-11-06 09:39:27 +0700

    Feat: API

---
+++ handler/account.go
@@ -0,0 +1,197 @@
+package handler
+
+import (
+	"net/http"
+	"task-golang-db/model"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type AccountInterface interface {
+	Create(*gin.Context)
+	Read(*gin.Context)
+	Update(*gin.Context)
+	Delete(*gin.Context)
+	List(*gin.Context)
+
+	My(*gin.Context)
+}
+
+type accountImplement struct {
+	db *gorm.DB
+}
+
+func NewAccount(db *gorm.DB) AccountInterface {
+	return &accountImplement{
+		db: db,
+	}
+}
+
+func (a *accountImplement) Create(c *gin.Context) {
+	payload := model.Account{}
+
+	// bind JSON Request to payload
+	err := c.BindJSON(&payload)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	// Create data
+	result := a.db.Create(&payload)
+	if result.Error != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": result.Error.Error(),
+		})
+		return
+	}
+
+	// Success response
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Create success",
+		"data":    payload,
+	})
+}
+
+func (a *accountImplement) Read(c *gin.Context) {
+	var account model.Account
+
+	// get id from url account/read/5, 5 will be the id
+	id := c.Param("id")
+
+	// Find first data based on id and put to account model
+	if err := a.db.First(&account, id).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+				"error": "Not found",
+			})
+			return
+		}
+
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// Success response
+	c.JSON(http.StatusOK, gin.H{
+		"data": account,
+	})
+}
+
+func (a *accountImplement) Update(c *gin.Context) {
+	payload := model.Account{}
+
+	// bind JSON Request to payload
+	err := c.BindJSON(&payload)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	// get id from url account/update/5, 5 will be the id
+	id := c.Param("id")
+
+	// Find first data based on id and put to account model
+	account := model.Account{}
+	result := a.db.First(&account, "account_id = ?", id)
+	if result.Error != nil {
+		if result.Error == gorm.ErrRecordNotFound {
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+				"error": "Not found",
+			})
+			return
+		}
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": result.Error.Error(),
+		})
+		return
+	}
+
+	// Update data
+	account.Name = payload.Name
+	a.db.Save(account)
+
+	// Success response
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Update success",
+	})
+}
+
+func (a *accountImplement) Delete(c *gin.Context) {
+	// get id from url account/delete/5, 5 will be the id
+	id := c.Param("id")
+
+	// Find first data based on id and delete it
+	if err := a.db.Where("account_id = ?", id).Delete(&model.Account{}).Error; err != nil {
+		// No data found and deleted
+		if err == gorm.ErrRecordNotFound {
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+				"error": "Not found",
+			})
+			return
+		}
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// Success response
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Delete success",
+		"data": map[string]string{
+			"account_id": id,
+		},
+	})
+}
+
+func (a *accountImplement) List(c *gin.Context) {
+	// Prepare empty result
+	var accounts []model.Account
+
+	// Find and get all accounts data and put to &accounts
+	if err := a.db.Find(&accounts).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// Success response
+	c.JSON(http.StatusOK, gin.H{
+		"data": accounts,
+	})
+}
+
+func (a *accountImplement) My(c *gin.Context) {
+	var account model.Account
+	// get account_id from middleware auth
+	accountID := c.GetInt64("account_id")
+
+	// Find first data based on account_id given
+	if err := a.db.First(&account, accountID).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+				"error": "Not found",
+			})
+			return
+		}
+
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// Success response
+	c.JSON(http.StatusOK, gin.H{
+		"data": account,
+	})
+}
---
+++ handler/auth.go
@@ -0,0 +1,181 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+	"task-golang-db/model"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
+type AuthInterface interface {
+	Login(*gin.Context)
+	Upsert(*gin.Context)
+}
+
+type authImplement struct {
+	db         *gorm.DB
+	signingKey []byte
+}
+
+func NewAuth(db *gorm.DB, signingKey []byte) AuthInterface {
+	return &authImplement{
+		db,
+		signingKey,
+	}
+}
+
+type authLoginPayload struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func (a *authImplement) Login(c *gin.Context) {
+	payload := authLoginPayload{}
+
+	// parsing JSON payload to struct model
+	err := c.BindJSON(&payload)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	// Validate username to get auth data
+	auth := model.Auth{}
+	if err := a.db.Where("username = ?",
+		payload.Username).
+		First(&auth).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error": "Login not valid",
+			})
+			return
+		}
+
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// Validate password
+	if err := bcrypt.CompareHashAndPassword([]byte(auth.Password), []byte(payload.Password)); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Login not valid",
+		})
+		return
+	}
+
+	// Login is valid
+	token, err := a.createJWT(&auth)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	// Success response
+	c.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("%v Login Sukses", payload.Username),
+		"data":    token,
+	})
+}
+
+type authUpsertPayload struct {
+	AccountID int64  `json:"account_id"`
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+}
+
+func (a *authImplement) Upsert(c *gin.Context) {
+	payload := authUpsertPayload{}
+
+	// parsing JSON payload to struct model
+	err := c.BindJSON(&payload)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	// Hash Given Password
+	hashed, err := bcrypt.GenerateFromPassword([]byte(payload.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	// Check AccountID is valid
+	var account model.Account
+	if err := a.db.First(&account, payload.AccountID).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+				"error": "Account Not found",
+			})
+			return
+		}
+
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// Prepare new auth data with new password
+	auth := model.Auth{
+		AccountID: payload.AccountID,
+		Username:  payload.Username,
+		Password:  string(hashed),
+	}
+
+	// Upsert auth data (Insert or Update if already exists)
+	result := a.db.Clauses(
+		clause.OnConflict{
+			DoUpdates: clause.AssignmentColumns([]string{"username", "password"}),
+			Columns:   []clause.Column{{Name: "account_id"}},
+		}).Create(&auth)
+	if result.Error != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": result.Error.Error(),
+		})
+		return
+	}
+
+	// Success response
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Create success",
+		"data":    payload.Username,
+	})
+}
+
+func (a *authImplement) createJWT(auth *model.Auth) (string, error) {
+	// Create the jwt token signer
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// Add claims data or additional data (avoid to put secret information in the payload or header elements)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["auth_id"] = auth.AuthID
+	claims["account_id"] = auth.AccountID
+	claims["username"] = auth.Username
+	claims["exp"] = time.Now().Add(time.Hour * 72).Unix() // Token expires in 72 hours
+
+	// Encode
+	tokenString, err := token.SignedString(a.signingKey)
+	if err != nil {
+		return "", err
+	}
+
+	// Return the token
+	return tokenString, nil
+}
